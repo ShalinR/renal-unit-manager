6b56437faea712099db554fcdc1df21773241763
anjulasenarath <s20515@sci.pdn.ac.lk>
2025-09-20 11:33:30 +0530

    feat(ward): scaffold Spring Boot server skeleton (web, data-jpa, mysql, lombok, validation)

---
+++ server/src/main/java/com/rcu/RcuServerApplication.java
@@ -0,0 +1,13 @@
+package com.rcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcuServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/rcu/RcuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
